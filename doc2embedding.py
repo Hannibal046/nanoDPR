ed60533efa0300c472bdad0ce705e414fdb36e27
hannibal046 <chengxin1998@stu.pku.edu.cn>
2023-10-16 18:05:13 +0000

    new

---
+++ doc2embedding.py
@@ -0,0 +1,54 @@
+import csv
+from tqdm import tqdm
+import os
+import transformers
+transformers.logging.set_verbosity_error()
+from transformers import DPRContextEncoder,DPRContextEncoderTokenizer
+import torch
+import numpy as np
+import pickle
+from accelerate import PartialState
+distributed_state = PartialState()
+device = distributed_state.device
+
+
+## load wikipedia passages
+num_docs = 21015324
+wikidata_path = "downloads/data/wikipedia_split/psgs_w100.tsv"
+progress_bar = tqdm(total=num_docs, disable=not distributed_state.is_main_process,ncols=100,desc='loading wikipedia...')
+id_col,text_col,title_col=0,1,2
+wikipedia = []
+with open(wikidata_path) as f:
+    reader = csv.reader(f, delimiter="\t")
+    for row in reader:
+        if row[id_col] == "id":continue
+        wikipedia.append(
+            [row[title_col],row[text_col].strip('"')]
+        )
+        progress_bar.update(1)
+
+
+model_name = 'facebook/dpr-ctx_encoder-single-nq-base'
+doc_encoder = DPRContextEncoder.from_pretrained(model_name)
+tokenizer = DPRContextEncoderTokenizer.from_pretrained(model_name)
+doc_encoder.eval()
+doc_encoder.to(device)
+
+with distributed_state.split_between_processes(wikipedia) as sharded_wikipedia:
+    batch_size = 1024
+    sharded_wikipedia = [sharded_wikipedia[idx:idx+batch_size] for idx in range(0,len(sharded_wikipedia),batch_size)]
+    encoding_progress_bar = tqdm(total=len(sharded_wikipedia), disable=not distributed_state.is_main_process,ncols=100,desc='encoding wikipedia...')
+    doc_embeddings = []
+    for data in sharded_wikipedia:
+        title = [x[0] for x in data]
+        passage = [x[1] for x in data]
+        model_input = tokenizer(title,passage,max_length=256,padding='max_length',return_tensors='pt',truncation=True).to(device)
+        with torch.no_grad():
+            output = doc_encoder(**model_input).pooler_output.cpu().numpy()
+        doc_embeddings.append(output)
+        encoding_progress_bar.update(1)
+    doc_embeddings = np.concatenate(doc_embeddings,axis=0)
+    os.makedirs("embedding/dpr-hf",exist_ok=True)
+    pickle.dump(doc_embeddings,open(f"embedding/dpr-hf/wikipedia_shard_{distributed_state.process_index}.embedding",'wb'))
+
+
---
+++ test_dpr.py
@@ -0,0 +1,101 @@
+from utils import normalize_query
+import csv
+import faiss,pickle        
+import numpy as np 
+from tqdm import tqdm
+from transformers import DPRQuestionEncoder,DPRQuestionEncoderTokenizer
+import torch
+from utils.tokenizers import SimpleTokenizer
+import unicodedata
+import time
+
+def normalize(text):
+    return unicodedata.normalize("NFD", text)
+
+def has_answer(answers,doc):
+    tokenizer = SimpleTokenizer()
+    doc = tokenizer.tokenize(normalize(doc)).words(uncased=True)
+    for answer in answers:
+        answer = tokenizer.tokenize(normalize(answer)).words(uncased=True)
+        for i in range(0, len(doc) - len(answer) + 1):
+                if answer == doc[i : i + len(answer)]:
+                    return True
+    return False
+
+if __name__ == '__main__':
+
+    ## load QA dataset
+    nq_test_file = "/home/v-xincheng/projects/llm-centered-retrieval/DPR/downloads/data/retriever/qas/nq-test.csv"
+    query_col,answers_col=0,1
+    queries,answers = [],[]
+    with open(nq_test_file) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            queries.append(normalize_query(row[query_col]))
+            answers.append(eval(row[answers_col]))
+    batch_size = 128
+    queries = [queries[idx:idx+batch_size] for idx in range(0,len(queries),batch_size)]
+    
+    # make faiss index
+    vector_sz = 768 
+    index = faiss.IndexFlatIP(vector_sz) 
+    for idx in tqdm(range(50),desc='building index from embedding...'):
+        index_path = f"DPR/dpr/downloads/data/retriever_results/nq/single/wikipedia_passages_{idx}.pkl"
+        data = pickle.load(open(index_path,'rb'))
+        data = [x[1] for x in data]
+        data = np.stack(data, axis=0)
+        index.add(data)  
+
+    ## load wikipedia passages
+    num_docs = 21015324
+    wikidata_path = "DPR/dpr/downloads/data/wikipedia_split/psgs_w100.tsv"
+    id_col,text_col,title_col=0,1,2
+    wiki_passages = []
+    with open(wikidata_path) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in tqdm(reader,total=num_docs,desc="loading wikipedia passages..."):
+            if row[id_col] == "id":continue
+            wiki_passages.append(row[text_col].strip('"'))
+    
+    ## load query encoder
+    model_name = "facebook/dpr-question_encoder-single-nq-base"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    query_encoder = DPRQuestionEncoder.from_pretrained(model_name).to(device)
+    toker = DPRQuestionEncoderTokenizer.from_pretrained(model_name)
+
+    ## embed queries
+    query_embeddings = []
+    for query in tqdm(queries,desc='encoding queries...'):
+        query_embedding = query_encoder(**toker(query,max_length=256,truncation=True,padding='max_length',return_tensors='pt').to(device))
+        query_embedding = query_embedding.pooler_output
+        query_embeddings.append(query_embedding.cpu().detach().numpy())
+    query_embeddings = np.concatenate(query_embeddings,axis=0)
+
+    ## retrieve top-k documents
+    print("searching index...",end=' ')
+    start_time = time.time()
+    top_k = 100
+    _,I = index.search(query_embeddings,top_k)
+    print(f"takes {time.time()-start_time} s")
+
+    hit_lists = []
+    for answer_list,id_list in tqdm(zip(answers,I),total=len(answers),desc='calculating metrics...'):
+        ## process single query
+        hit_list = []
+        for doc_id in id_list:
+            doc = wiki_passages[doc_id]
+            hit_list.append(has_answer(answer_list,doc))
+        hit_lists.append(hit_list)
+
+    top_k_hits = [0]*top_k
+    best_hits = []
+    for hit_list in hit_lists:
+        best_hit = next((i for i, x in enumerate(hit_list) if x), None)
+        if best_hit is not None:
+            top_k_hits[best_hit:] = [v + 1 for v in top_k_hits[best_hit:]]
+    
+    top_k_ratio = [x/len(answers) for x in top_k_hits]
+    
+    for idx in range(top_k):
+        if (idx+1) % 10 == 0:
+            print(f"top-{idx+1} accuracy",top_k_ratio[idx])
