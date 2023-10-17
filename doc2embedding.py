11fc31daac635ef18e0e38fce242c3693e9d7a47
hannibal046 <chengxin1998@stu.pku.edu.cn>
2023-10-17 16:54:44 +0000

    v.0.1

--- doc2embedding.py
+++ doc2embedding.py
@@ -3,52 +3,67 @@ from tqdm import tqdm
 import os
 import transformers
 transformers.logging.set_verbosity_error()
-from transformers import DPRContextEncoder,DPRContextEncoderTokenizer
+from transformers import (
+    DPRContextEncoder,
+    DPRContextEncoderTokenizer,
+    BertTokenizer,
+    BertModel,
+    )
 import torch
 import numpy as np
 import pickle
 from accelerate import PartialState
-distributed_state = PartialState()
-device = distributed_state.device
+
+if __name__ == "__main__":
+
+    ## define args
+    wikidata_path = "downloads/data/wikipedia_split/psgs_w100.tsv"
+    NUM_DOCS = 21015324
+    PreTrainedModelClass = BertModel
+    PreTrainedTokenizerClass = BertTokenizer
+    pretrained_model_name = "wandb/run-20231016_181105-e04snomn/files/step-18400/doc_encoder"  #'facebook/dpr-ctx_encoder-single-nq-base'
+    output_dir = "embedding/dpr-own"
+    
+    distributed_state = PartialState()
+    device = distributed_state.device
+
+    ## load encoder
+    doc_encoder = PreTrainedModelClass.from_pretrained(pretrained_model_name,add_pooling_layer=False)
+    tokenizer = PreTrainedTokenizerClass.from_pretrained('bert-base-uncased')
+    doc_encoder.eval()
+    doc_encoder.to(device)
 
 
-## load wikipedia passages
-num_docs = 21015324
-wikidata_path = "downloads/data/wikipedia_split/psgs_w100.tsv"
-progress_bar = tqdm(total=num_docs, disable=not distributed_state.is_main_process,ncols=100,desc='loading wikipedia...')
-id_col,text_col,title_col=0,1,2
-wikipedia = []
-with open(wikidata_path) as f:
-    reader = csv.reader(f, delimiter="\t")
-    for row in reader:
-        if row[id_col] == "id":continue
-        wikipedia.append(
-            [row[title_col],row[text_col].strip('"')]
-        )
-        progress_bar.update(1)
+    ## load wikipedia passages
+    progress_bar = tqdm(total=NUM_DOCS, disable=not distributed_state.is_main_process,ncols=100,desc='loading wikipedia...')
+    id_col,text_col,title_col=0,1,2
+    wikipedia = []
+    with open(wikidata_path) as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            if row[id_col] == "id":continue
+            wikipedia.append(
+                [row[title_col],row[text_col].strip('"')]
+            )
+            progress_bar.update(1)
 
-
-model_name = 'facebook/dpr-ctx_encoder-single-nq-base'
-doc_encoder = DPRContextEncoder.from_pretrained(model_name)
-tokenizer = DPRContextEncoderTokenizer.from_pretrained(model_name)
-doc_encoder.eval()
-doc_encoder.to(device)
-
-with distributed_state.split_between_processes(wikipedia) as sharded_wikipedia:
-    batch_size = 1024
-    sharded_wikipedia = [sharded_wikipedia[idx:idx+batch_size] for idx in range(0,len(sharded_wikipedia),batch_size)]
-    encoding_progress_bar = tqdm(total=len(sharded_wikipedia), disable=not distributed_state.is_main_process,ncols=100,desc='encoding wikipedia...')
-    doc_embeddings = []
-    for data in sharded_wikipedia:
-        title = [x[0] for x in data]
-        passage = [x[1] for x in data]
-        model_input = tokenizer(title,passage,max_length=256,padding='max_length',return_tensors='pt',truncation=True).to(device)
-        with torch.no_grad():
-            output = doc_encoder(**model_input).pooler_output.cpu().numpy()
-        doc_embeddings.append(output)
-        encoding_progress_bar.update(1)
-    doc_embeddings = np.concatenate(doc_embeddings,axis=0)
-    os.makedirs("embedding/dpr-hf",exist_ok=True)
-    np.save(f'embedding/dpr-hf/wikipedia_shard_{distributed_state.process_index}.npy',doc_embeddings)
+    with distributed_state.split_between_processes(wikipedia) as sharded_wikipedia:
+        batch_size = 1024
+        sharded_wikipedia = [sharded_wikipedia[idx:idx+batch_size] for idx in range(0,len(sharded_wikipedia),batch_size)]
+        encoding_progress_bar = tqdm(total=len(sharded_wikipedia), disable=not distributed_state.is_main_process,ncols=100,desc='encoding wikipedia...')
+        doc_embeddings = []
+        for data in sharded_wikipedia:
+            title = [x[0] for x in data]
+            passage = [x[1] for x in data]
+            model_input = tokenizer(title,passage,max_length=256,padding='max_length',return_tensors='pt',truncation=True).to(device)
+            with torch.no_grad():
+                # output = doc_encoder(**model_input).pooler_output.cpu().numpy()
+                CLS_POS = 0
+                output = doc_encoder(**model_input).last_hidden_state[:,CLS_POS,:].cpu().numpy()
+            doc_embeddings.append(output)
+            encoding_progress_bar.update(1)
+        doc_embeddings = np.concatenate(doc_embeddings,axis=0)
+        os.makedirs(output_dir,exist_ok=True)
+        np.save(f'{output_dir}/wikipedia_shard_{distributed_state.process_index}.npy',doc_embeddings)
 
 
--- train_dpr.py
+++ train_dpr.py
@@ -49,7 +49,7 @@ class DualEncoder(nn.Module):
         super().__init__()
         self.query_encoder = query_encoder
         self.doc_encoder = doc_encoder
-    
+
     def forward(
         self,
         query_input_ids, # [bs,seq_len]
@@ -59,21 +59,22 @@ class DualEncoder(nn.Module):
         doc_attention_mask, # [bs*n_doc,seq_len]
         doc_token_type_ids, # [bs*n_doc,seq_len]
     ):  
-        CLS_POS=0
         ## [bs,n_dim]
+        CLS_POS = 0
         query_embedding = self.query_encoder(
             input_ids=query_input_ids,
             attention_mask = query_attention_mask,
             token_type_ids = query_token_type_ids,
             ).last_hidden_state[:,CLS_POS,:]
+        
         ## [bs * n_doc,n_dim]
         doc_embedding = self.doc_encoder(
             input_ids = doc_input_ids,
             attention_mask = doc_attention_mask,
             token_type_ids = doc_token_type_ids,
             ).last_hidden_state[:,CLS_POS,:]
+        
         return query_embedding,doc_embedding 
-
 
 def calculate_dpr_loss(matching_score,labels):
     return F.nll_loss(input=F.log_softmax(matching_score,dim=1),target=labels)
@@ -82,11 +83,14 @@ def calculate_hit_cnt(matching_score,labels):
     _, max_ids = torch.max(matching_score,1)
     return (max_ids == labels).sum()
 
-def calculate_average_rank(matching_score):
-    positive_doc_position = 0
+def calculate_average_rank(matching_score,labels):
     _,indices = torch.sort(matching_score,dim=1,descending=True)
-    ranks = torch.nonzero(torch.eq(indices,positive_doc_position),as_tuple=True)[1] + 1 ##  rank starts from 1
-    return ranks.tolist()
+    ranks = []
+    for idx,label in enumerate(labels):
+        rank = ((indices[idx] == label).nonzero()).item() + 1  ##  rank starts from 1
+        ranks.append(rank)
+    return ranks
+
 
 class QADataset(torch.utils.data.Dataset):
     def __init__(self,file_path):
@@ -110,28 +114,22 @@ class QADataset(torch.utils.data.Dataset):
         ## passage = title + document
         positive_passages = [x['positive_ctxs'][0] for x in samples]
         positive_titles = [x['title'] for x in positive_passages]
-        positive_docs = [normalize_document(x['text']) for x in positive_passages]
+        positive_docs = [x['text'] for x in positive_passages]
 
         if stage == 'train':
             ## random choose one negative document
-            negative_passages = [ random.choice(x['hard_negative_ctxs']) 
+            negative_passages = [random.choice(x['hard_negative_ctxs']) 
                                  if len(x['hard_negative_ctxs']) != 0  
                                  else random.choice(x['negative_ctxs']) 
                                  for x in samples ]
         elif stage == 'dev':
-            negative_passages = [random.sample(
-                                               x['hard_negative_ctxs'],
-                                               min(args.num_hard_negative_ctx,len(x['hard_negative_ctxs']))
-                                               )
-                                +random.sample(
-                                               x['negative_ctxs'],
-                                               min(args.num_other_negative_ctx,len(x['negative_ctxs']))
-                                               )
+            negative_passages = [x['hard_negative_ctxs'][:min(args.num_hard_negative_ctx,len(x['hard_negative_ctxs']))]
+                                + x['negative_ctxs'][:min(args.num_other_negative_ctx,len(x['negative_ctxs']))] 
                                 for x in samples]
             negative_passages = [x for y in negative_passages for x in y]
 
         negative_titles = [x["title"] for x in negative_passages]
-        negative_docs = [normalize_document(x["text"]) for x in negative_passages]
+        negative_docs = [x["text"] for x in negative_passages]
         titles = positive_titles + negative_titles
         docs = positive_docs + negative_docs
 
@@ -149,25 +147,35 @@ class QADataset(torch.utils.data.Dataset):
 
 def validate(model,dataloader,args,accelerator):
     model.eval()
-    ranks,losses,hit_cnts = [],[],[]
+    query_embeddings = []
+    positive_doc_embeddings = []
+    negative_doc_embeddings = []
     for batch in dataloader:
         with torch.no_grad():
             query_embedding,doc_embedding  = model(**batch)
-        bs,d_model = query_embedding.shape
-        doc_embedding = doc_embedding.view(bs,-1,d_model)
-        matching_score = torch.bmm(query_embedding.unsqueeze(1),doc_embedding.permute(0,2,1)).squeeze(1) # bs, num_pos+num_neg
-        labels = torch.zeros(bs,dtype=torch.int64).to(matching_score.device)
-        loss = calculate_dpr_loss(matching_score,labels=labels)
-        # hit_cnt = calculate_hit_cnt(matching_score,labels=labels)
-        rank = calculate_average_rank(matching_score)
-        ranks.extend(rank)
-        losses.append(loss.item())
-    
-    if accelerator.use_distributed:
-        ranks = accelerator.gather_for_metrics(ranks)
-        losses = accelerator.gather_for_metrics(losses)
+        query_num,_ = query_embedding.shape
+        query_embeddings.append(query_embedding.cpu())
+        positive_doc_embeddings.append(doc_embedding[:query_num,:].cpu())
+        negative_doc_embeddings.append(doc_embedding[query_num:,:].cpu())
 
-    return sum(ranks)/len(ranks),sum(losses)/len(losses)
+    query_embeddings = torch.cat(query_embeddings,dim=0)
+    doc_embeddings = torch.cat(positive_doc_embeddings+negative_doc_embeddings,dim=0)
+    matching_score = torch.matmul(query_embeddings,doc_embeddings.permute(1,0)) # bs, num_pos+num_neg
+    labels = torch.arange(query_embeddings.shape[0],dtype=torch.int64).to(matching_score.device)
+    loss = calculate_dpr_loss(matching_score,labels=labels).item()
+    # hit_cnt = calculate_hit_cnt(matching_score,labels=labels)
+    ranks = calculate_average_rank(matching_score,labels=labels)
+    
+    if accelerator.use_distributed and accelerator.num_processes>1:
+        ranks_from_all_gpus = [None for _ in range(accelerator.num_processes)] 
+        dist.all_gather_object(ranks_from_all_gpus,ranks)
+        ranks = [x for y in ranks_from_all_gpus for x in y]
+
+        loss_from_all_gpus = [None for _ in range(accelerator.num_processes)] 
+        dist.all_gather_object(loss_from_all_gpus,loss)
+        loss = sum(loss_from_all_gpus)/len(loss_from_all_gpus)
+    
+    return sum(ranks)/len(ranks),loss
 
 def main():
     args = parse_args()
@@ -192,13 +200,15 @@ def main():
     query_encoder = BertModel.from_pretrained(args.base_model,add_pooling_layer=False)
     doc_encoder = BertModel.from_pretrained(args.base_model,add_pooling_layer=False)
     dual_encoder = DualEncoder(query_encoder,doc_encoder)
+    dual_encoder.train()
 
     train_dataset = QADataset(args.train_file)
     train_collate_fn = functools.partial(QADataset.collate_fn,tokenizer=tokenizer,stage='train',args=args,)
     train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size=args.per_device_train_batch_size,shuffle=True,collate_fn=train_collate_fn,num_workers=4,pin_memory=True)
+    
     dev_dataset = QADataset(args.dev_file)
     dev_collate_fn = functools.partial(QADataset.collate_fn,tokenizer=tokenizer,stage='dev',args=args,)
-    dev_dataloader = torch.utils.data.DataLoader(dev_dataset,batch_size=args.per_device_eval_batch_size,shuffle=True,collate_fn=dev_collate_fn,num_workers=4,pin_memory=True)
+    dev_dataloader = torch.utils.data.DataLoader(dev_dataset,batch_size=args.per_device_eval_batch_size,shuffle=False,collate_fn=dev_collate_fn,num_workers=4,pin_memory=True)
 
     
     no_decay = ["bias", "LayerNorm.weight"]
@@ -236,55 +246,64 @@ def main():
     logger.info(f"  Total optimization steps = {MAX_TRAIN_STEPS}")
     logger.info(f"  Per device eval batch size = {args.per_device_eval_batch_size}")
     completed_steps = 0
-    progress_bar = tqdm(range(MAX_TRAIN_STEPS), disable=not accelerator.is_local_main_process,ncols=100)
+    # progress_bar = tqdm(range(MAX_TRAIN_STEPS), disable=not accelerator.is_local_main_process,ncols=100)
+    progress_bar = tqdm(range(MAX_TRAIN_STEPS), disable=True, ncols=100)
 
     for epoch in range(MAX_TRAIN_EPOCHS):
         set_seed(args.seed+epoch)
         progress_bar.set_description(f"epoch: {epoch+1}/{MAX_TRAIN_EPOCHS}")
-        dual_encoder.train()
         for step,batch in enumerate(train_dataloader):
             with accelerator.accumulate(dual_encoder):
-                query_embedding,doc_embedding  = dual_encoder(**batch)
                 with accelerator.autocast():
-                    bs,_ = query_embedding.shape
-                    if accelerator.use_distributed: 
+                    query_embedding,doc_embedding  = dual_encoder(**batch)
+                    single_device_query_num,_ = query_embedding.shape
+                    single_device_doc_num,_ = doc_embedding.shape
+                    if accelerator.use_distributed:
                         doc_list = [torch.zeros_like(doc_embedding) for _ in range(accelerator.num_processes)]
                         dist.all_gather(tensor_list=doc_list, tensor=doc_embedding.contiguous())
                         doc_list[dist.get_rank()] = doc_embedding
-                        doc_embedding = torch.cat(doc_list, 0)
+                        doc_embedding = torch.cat(doc_list, dim=0)
+
+                        query_list = [torch.zeros_like(query_embedding) for _ in range(accelerator.num_processes)]
+                        dist.all_gather(tensor_list=query_list, tensor=query_embedding.contiguous())
+                        query_list[dist.get_rank()] = query_embedding
+                        query_embedding = torch.cat(query_list, dim=0)
+
                     matching_score = torch.matmul(query_embedding,doc_embedding.permute(1,0))
-                    labels = torch.arange(start=bs*GPU_INDEX,end=bs*(GPU_INDEX+1)).to(matching_score.device)
-                    
+                    labels = torch.cat([torch.arange(single_device_query_num) + gpu_index * single_device_doc_num for gpu_index in range(accelerator.num_processes)],dim=0).to(matching_score.device)
                     loss = calculate_dpr_loss(matching_score,labels=labels)
-                    hit_cnt = calculate_hit_cnt(matching_score,labels=labels)
+
                 accelerator.backward(loss)
-                optimizer.step()
-                optimizer.zero_grad()
 
                 ## one optimization step
                 if accelerator.sync_gradients:
                     progress_bar.update(1)
-                    progress_bar.set_postfix(loss=f"{loss:.2f}")
+                    progress_bar.set_postfix(loss=f"{loss:.4f}",lr=f"{lr_scheduler.get_last_lr()[0]:6f}")
                     completed_steps += 1
                     accelerator.clip_grad_norm_(dual_encoder.parameters(), args.max_grad_norm)
                     if not accelerator.optimizer_step_was_skipped:
                         lr_scheduler.step()
+                    logger.info(f"step = {completed_steps} loss = {loss}")
                     accelerator.log({"training_loss": loss}, step=completed_steps)
                     accelerator.log({"lr": lr_scheduler.get_last_lr()[0]}, step=completed_steps)
                     
                     if completed_steps % EVAL_STEPS == 0:
                         avg_rank,loss = validate(dual_encoder,dev_dataloader,args,accelerator)
+                        dual_encoder.train()
                         accelerator.log({"avg_rank": avg_rank, "loss":loss}, step=completed_steps)
                         accelerator.wait_for_everyone()
                         if accelerator.is_local_main_process:
                             unwrapped_model = accelerator.unwrap_model(dual_encoder)
-                            unwrapped_model.query_encoder.save_pretrained(
-                                os.path.join(LOG_DIR,f"step-{completed_steps}/query_encoder")
-                            )
-                            unwrapped_model.doc_encoder.save_pretrained(
-                                os.path.join(LOG_DIR,f"step-{completed_steps}/doc_encoder")
-                            )
+                            unwrapped_model.query_encoder.save_pretrained(os.path.join(LOG_DIR,f"step-{completed_steps}/query_encoder"))
+                            tokenizer.save_pretrained(os.path.join(LOG_DIR,f"step-{completed_steps}/query_encoder"))
+                            
+                            unwrapped_model.doc_encoder.save_pretrained(os.path.join(LOG_DIR,f"step-{completed_steps}/doc_encoder"))
+                            tokenizer.save_pretrained(os.path.join(LOG_DIR,f"step-{completed_steps}/doc_encoder"))
+
                         accelerator.wait_for_everyone()
+                
+                optimizer.step()
+                optimizer.zero_grad()
     
     if accelerator.is_local_main_process:wandb_tracker.finish()
     accelerator.end_training()
